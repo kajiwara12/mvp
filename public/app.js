3f3427f0dbfb827c8ab621290d6c512af368a146
Quinn Kaji <quinnkaji@gmail.com>
2023-10-20 10:18:23 -1000

    more changes

--- public/app.js
+++ public/app.js
@@ -13,25 +13,30 @@ let rndLngth = generateNumber(5, 8);
 for (let i = 65; i <= 90; i++) {
   let letter = document.createElement("button");
   letter.innerText = String.fromCharCode(i);
+  console.log(String.fromCharCode(i));
   letter.classList.add("letter");
   letter.addEventListener("click", checkLetter);
   letterArea.appendChild(letter);
 }
-// create 5-7 letter word in game---------------------------------------
+//create blank spots based on word length && create 5-7 letter word in game---------------------------------------
 function getWord() {
   fetch(`https://random-word-api.herokuapp.com/word?length=${rndLngth}&lang=en`)
     .then((response) => {
       return response.json();
     })
     .then((word) => {
+      let wordWord = word[0];
       let newWord = document.createElement("p");
-      needDef = word;
-      console.log(needDef);
-      newWord.innerText = word;
+      newWord.innerText = wordWord.toUpperCase();
       gameArea.append(newWord);
+      for (let i = 0; i < rndLngth; i++) {
+        let spot = document.createElement("p");
+        spot.classList.add("spot");
+        spot.innerText = wordWord[i].toUpperCase();
+        gameArea.append(spot);
+      }
     });
 }
-
 function checkLetter(event) {}
 
 fetch("/player")
